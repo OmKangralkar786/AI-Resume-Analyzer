d724bb17da4fab258c7b4f36fd6cf339b3b8c357
OmKangralkar786 <omshrikantkangralkar1899@gmail.com>
2026-06-05 14:18:14 +0530

    Enable debug

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-k0$5kbm=1#y4&w)5pj2ts39p9=(e7g-qvp!v)suks8m2nxe-ao"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
